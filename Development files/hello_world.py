e9d603d86c4f3d8f1e78266657b73860a9d37c1b
Matthew Hill <mhill@student.saintaug.nsw.edu.au>
2024-04-09 09:26:48 +1000

    Getting setup with my repo

---
+++ Development files/hello_world.py
@@ -0,0 +1,8 @@
+#1 - set up GIT locally 
+#2 - set up your GIT email and username using the GIT commands 
+    #set username: git config --global user.name 
+    #set email: git config --global user.email
+#3 - create github account, pfp, school email
+#4 - log into github within VScode 
+#5 - setup a development folder somewhere on your computer 
+#6 - open a folder within VScode File > open folder > Major Work Folder
