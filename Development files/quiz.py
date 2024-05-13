3dec7812d26c416d66554172f96983c7db2374e5
Matthew Hill <mhill@student.saintaug.nsw.edu.au>
2024-05-13 15:21:34 +1000

    trying buttons but not working too well

--- Development files/quiz.py
+++ Development files/quiz.py
@@ -8,25 +8,49 @@ screen = pygame.display.set_mode((1200, 700))
 clock = pygame.time.Clock()
 running = True
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white") 
-
-    # setting/changing icon & name of pygame window
-    pygame.display.set_caption('St Augs Quiz')
-    Icon = pygame.image.load('Staugs.jpeg')
-    pygame.display.set_icon(Icon)
+  # setting/changing icon & name of pygame window
+pygame.display.set_caption('St Augs Quiz')
+Icon = pygame.image.load('Staugs.jpeg')
+pygame.display.set_icon(Icon)
 
 
     # Load button images
-    start_img = pygame.image.load('start_btn.png').convert_alpha
-    exit_img = pygame.image.load('start_btn.png').convert_alpha
+start_img = pygame.image.load('start_btn.png').convert_alpha
+exit_img = pygame.image.load('exit_btn.png').convert_alpha
+
+    # button class 
+class button(): 
+        def __init__(self, x, y, image):
+            self.image = image
+            self.rect = self.image.get_rect() 
+            self.rect.topleft = (x, y)
+
+        
+        def draw(self):
+            #draw button on screen
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    #create button instances 
+start_button = button(100, 200, start_img)  
+exit_button = button(450, 200, exit_img) 
+
+#game loop
+run = True
+while run:
+
+    screen.fill((202, 228, 241)) 
+    
+    start_button.draw()
+    exit_button.draw()
+    
+    #event handler
+    for event in pygame.event.get():
+         #quit game
+         if event.type == pygame.QUIT:
+              run = False
+
+
 
     # #set colour
     # colour = (5, 102, 8)
@@ -37,9 +61,7 @@ while running:
     # pygame.draw.rect(screen, colour, pygame.Rect(100, 450, 400, 200),  100, 10)
     # pygame.draw.rect(screen, colour, pygame.Rect(700, 450, 400, 200),  100, 10)
 
-    clock.tick(60)  # limits FPS to 60  
+#     clock.tick(60)  # limits FPS to 60  
 
-# flip() the display to put your work on screen
-    pygame.display.flip()
-
-    
+# # flip() the display to put your work on screen
+#     pygame.display.flip()
