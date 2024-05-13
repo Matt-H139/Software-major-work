40ce15a796bd5c46594dec5fc9c31fe54f988e6b
Matthew Hill <mhill@student.saintaug.nsw.edu.au>
2024-05-14 09:50:41 +1000

    set up buttons with print functions

--- Development files/multi choice tk.py
+++ Development files/multi choice tk.py
@@ -1,16 +1,41 @@
 import customtkinter
 
 
-class page():
+class page(): 
     def __init__(self):
-         self.app = customtkinter.CTk("Mutiple Choice Quiz") #creating the app 
+         self.app = customtkinter.CTk() #creating the app 
          self.frame = customtkinter.CTkFrame(self.app)
          self.app.geometry("400x400") 
-         customtkinter.set_appearance_mode('light')
-         customtkinter.set_default_color_theme('dark green') 
+         customtkinter.set_appearance_mode('light') #setting appreance mode, light/dark
+         customtkinter.set_default_color_theme('green') 
 
          self.frame.pack(pady=20, padx=60, fill='both', expand=True)
 
+         self.button = customtkinter.CTkButton(self.app, text='A', command=self.button_print_answer) 
+         
 
          for i in range (12):
-              self.frame.grid_columnconfigure
+              self.frame.grid_columnconfigure(i, weight=1,) # setting grid, columns 
+         for i in range (12):
+                   self.frame.grid_rowconfigure(i, weight=1,) # rows
+        
+         self.button.pack(padx=1, pady=1) 
+
+
+
+    def button_print_answer(self):
+        print('option a')
+
+    
+
+
+
+    def run(self):
+          self.app.mainloop()
+
+   
+
+
+
+Running = page()
+Running.run() 
