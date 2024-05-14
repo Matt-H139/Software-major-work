cf1eabd2d8efa9c612422eea9102b532b9b4c85c
Matthew Hill <mhill@student.saintaug.nsw.edu.au>
2024-05-14 12:30:12 +1000

    set up columns and rows properly

--- Development files/multi choice tk.py
+++ Development files/multi choice tk.py
@@ -5,21 +5,21 @@ class page():
     def __init__(self):
          self.app = customtkinter.CTk() #creating the app 
          self.frame = customtkinter.CTkFrame(self.app)
-         self.app.geometry("400x400") 
-         customtkinter.set_appearance_mode('light') #setting appreance mode, light/dark
-         customtkinter.set_default_color_theme('green') 
+         self.app.geometry("500x500") 
+         customtkinter.set_appearance_mode('dark') #setting appreance mode, light/dark
+         customtkinter.set_default_color_theme('blue') 
 
          self.frame.pack(pady=20, padx=60, fill='both', expand=True)
 
-         self.button = customtkinter.CTkButton(self.app, text='A', command=self.button_print_answer) 
+         self.button = customtkinter.CTkButton(master=self.frame, text='A', command=self.button_print_answer) 
          
 
-         for i in range (12):
-              self.frame.grid_columnconfigure(i, weight=1,) # setting grid, columns 
-         for i in range (12):
+         for i in range (7):
+              self.frame.grid_columnconfigure(i, weight=3,) # setting grid, columns 
+         for i in range (7):
                    self.frame.grid_rowconfigure(i, weight=1,) # rows
         
-         self.button.pack(padx=1, pady=1) 
+         self.button.grid(row=1, column=4, padx=1, pady=1) 
 
 
 
