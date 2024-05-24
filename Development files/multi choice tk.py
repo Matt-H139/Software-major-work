834ba6d5691db0a47ae832fb59e840780050c157
Matthew Hill <mhill@student.saintaug.nsw.edu.au>
2024-05-24 14:12:49 +1000

    changed textbox to label for better look in window

--- Development files/multi choice tk.py
+++ Development files/multi choice tk.py
@@ -9,13 +9,12 @@ class Page(customtkinter.CTk):  # Inherit from customtkinter.CTk
         customtkinter.set_default_color_theme("blue")  # Setting theme
         
         self.frame = customtkinter.CTkFrame(self)
-        self.frame.pack(pady=20, padx=60, fill='both', expand=True)
+        self.frame.pack(pady=20, padx=20, fill='both', expand=True)
         
-        self.textbox = customtkinter.CTkTextbox(master=self.frame, width=200, corner_radius=0)
-        self.textbox.insert('0.0', 'example text')
-        self.textbox.grid(row=0, column=4, sticky='nsew')
+        self.label = customtkinter.CTkLabel(master=self.frame, text='Quiz Title', fg_color='transparent') #setting label dimensions
+        self.label.grid(row=0, column=4, sticky='nsew') #label in grid
         
-        self.buttonA = customtkinter.CTkButton(master=self.frame, text='A', command=self.button_print_answerA)
+        self.buttonA = customtkinter.CTkButton(master=self.frame, text='A', command=self.button_print_answerA) #button commands + labels
         self.buttonB = customtkinter.CTkButton(master=self.frame, text='B', command=self.button_print_answerB)
         self.buttonC = customtkinter.CTkButton(master=self.frame, text='C', command=self.button_print_answerC)
         self.buttonD = customtkinter.CTkButton(master=self.frame, text='D', command=self.button_print_answerD)
@@ -24,12 +23,12 @@ class Page(customtkinter.CTk):  # Inherit from customtkinter.CTk
             self.frame.grid_columnconfigure(i, weight=3)
             self.frame.grid_rowconfigure(i, weight=1)
         
-        self.buttonA.grid(row=4, column=0, padx=1, pady=1)
+        self.buttonA.grid(row=4, column=0, padx=1, pady=1) #setting buttons in grid
         self.buttonB.grid(row=4, column=8, padx=1, pady=1)
         self.buttonC.grid(row=8, column=0, padx=1, pady=1)
         self.buttonD.grid(row=8, column=8, padx=1, pady=1)
 
-    def button_print_answerA(self):
+    def button_print_answerA(self):   # button commands 
         print('option a')
     
     def button_print_answerB(self):
