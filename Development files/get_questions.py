a69ad13eb04af4f3c495d1afa5c5fc58c3776c7d
Matthew Hill <mhill@student.saintaug.nsw.edu.au>
2024-06-13 09:18:17 +1000

    quiz data imported into quiz as questions

--- Development files/get_questions.py
+++ Development files/get_questions.py
@@ -1,20 +1,29 @@
 import pandas as pd
 
 
-# Load the data from the CSV file
-df = pd.read_csv('questions.csv')
 
-quiz_data = []
-for index, row in df.iterrows():
-    question_dict = {
-        "subject": row["subject"],
-        "question": row["question"] if pd.notna(row["question"]) else "No question provided",
-        "options": [row["option_a"], row["option_b"], row["option_c"], row["option_d"]],
-        "correct": row["answer"]
-    }
-    quiz_data.append(question_dict)
 
-# Print the quiz data to verify
-for question in quiz_data:
-    if question["subject"] == "Physics":
-        print(question)
+def import_quiz_data():
+    # Load the data from the CSV file
+    df = pd.read_csv('questions.csv')
+
+    quiz_data = []
+    for index, row in df.iterrows():
+        question_dict = {
+            "subject": row["subject"],
+            "question": row["question"] if pd.notna(row["question"]) else "No question provided",
+            "options": [row["option_a"], row["option_b"], row["option_c"], row["option_d"]],
+            "correct": row["answer"]
+        }
+        quiz_data.append(question_dict)
+
+    # # Print the quiz data to verify
+    # for question in quiz_data:
+    #     if question["subject"] == "Physics":
+    #         print(question)
+
+    return quiz_data
+
+
+
+# step 1 - return quiz_data so we can use it in multichoiceuk
