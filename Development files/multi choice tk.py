519506579e85461346c11eaee0a5f43d1e080e23
Matthew Hill <mhill@student.saintaug.nsw.edu.au>
2024-06-13 12:19:38 +1000

    still working on subject choice working with quiz

--- Development files/multi choice tk.py
+++ Development files/multi choice tk.py
@@ -8,6 +8,47 @@ class Page(customtkinter.CTk):  # Inherit from customtkinter.CTk
     def __init__(self):
         super().__init__()  # Initialize the superclass
         
+        self.geometry('500x500')
+        customtkinter.set_appearance_mode("dark")
+        customtkinter.set_default_color_theme('blue')
+
+        self.frame2 = customtkinter.CTkFrame(self)
+        self.frame2.pack(pady =10, padx =10, fill='both', expand=True)
+
+        self.Label2 = customtkinter.CTkLabel(master=self.frame2, text='Select Quiz Topic', fg_color='transparent')
+        self.Label2.grid(row=0, column=3, sticky='nsew')
+
+        self.options_buttons2 = []
+        for i in range(4):
+            Button1 = customtkinter.CTkButton(master=self.frame2, text='', command=lambda i=i: self.check_answer(i))
+            self.options_buttons2.append(Button1) 
+            Button1.grid(row=i + 1, column=1, pady=5, padx=5)
+
+        for i in range(10):
+            self.frame2.grid_columnconfigure(i, weight=3)
+            self.frame2.grid_rowconfigure(i, weight=1)
+
+
+        self.questions = [
+            {
+                'question': 'Select Quiz Topic',
+                'options': ['physics', 'biology', 'mathematics', 'italian'], 
+                'correct': 0
+            }
+        ]
+        
+
+        self.current_question_index = 0 
+        # self.load_question()
+
+    def load_question(self):
+        question_data = self.questions[self.current_question_index]
+        options = question_data['options']
+        for i, option in enumerate(options):
+            self.options_buttons[i].configure(text=option)
+
+
+
         self.geometry("750x750") # Window size
         customtkinter.set_appearance_mode("dark")  # Setting appearance mode
         customtkinter.set_default_color_theme("blue")  # Setting theme
