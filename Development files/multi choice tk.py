f7d3762ab33f715ac63c00c478d8733fc3e4a9ca
Matthew Hill <mhill@student.saintaug.nsw.edu.au>
2024-06-18 12:12:22 +1000

    Shows when quiz is complete and shows score

--- Development files/multi choice tk.py
+++ Development files/multi choice tk.py
@@ -5,8 +5,9 @@ global quiz_data
 
 quiz_data = import_quiz_data()
 
+global score
 
-
+score = 0 
 
 
 class Page(customtkinter.CTk):  # Inherit from customtkinter.CTk
@@ -67,6 +68,7 @@ class Page(customtkinter.CTk):  # Inherit from customtkinter.CTk
         self.feedback_label.grid(row=1, column=4, sticky='nsew') #label in grid
         
         self.option_buttons = []
+
         for i in range(4):
             buttonA = customtkinter.CTkButton(master=self.frame, text='A', command=lambda i=i: self.check_answer(i)) #button commands + labels
             self.option_buttons.append(buttonA)    
@@ -91,21 +93,30 @@ class Page(customtkinter.CTk):  # Inherit from customtkinter.CTk
             self.option_buttons[i].configure(text=option)
 
     def check_answer(self, selected_option_index):
-        correct_option_index = self.questions[self.current_question_index]["correct"]
+
+        
+        correct_option_index = self.questions[self.current_question_index]["correct"] # accesses the correct answer for the question
+        print("Test",correct_option_index)
+
+        print(self.questions[self.current_question_index]["correct"])
 
         if selected_option_index == correct_option_index:
-            self.feedback_label.configure(text="Correct!", text_color="green")
-        else:
-            self.feedback_label.configure(text="Incorrect!", text_color="red")
+            score=score+1
+            # self.feedback_label.configure(text="Correct!", text_color="green")
+        # else:
+        #     self.feedback_label.configure(text="Incorrect!", text_color="red")
 
         self.current_question_index += 1
+
         if self.current_question_index < len(self.questions):
             self.load_question()
         else:
-            self.feedback_label.configure(text="Quiz Completed!")
+            
             for button in self.option_buttons:
                 button.configure(state='disabled')
-            self.feedback_label.configure(text='')
+            self.feedback_label.configure(text=f"Quiz Completed!\n Your score:  ")
+            print(score)
+
 
 def run():
     app = Page()
